72ec7189c50e0e2d76bc9de44f734f9469e51445
Aman Kushwaha <ashayar630@gmail.com>
2026-07-25 20:32:36 +0530

    Just Code

---
+++ SuM.java
@@ -0,0 +1,15 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+import java.util.Scanner;
+public class SuM{
+    public static void main(String []args){
+        Scanner Sc=new Scanner(System.in);
+        System.out.println("Enter the Value of a :");
+        int a=Sc.nextInt();
+        System.out.println("Enter the Value of b :");
+        int b=Sc.nextInt();
+        int Sum=a+b;
+        System.out.println("Sum : "+Sum);
+    }
+}
