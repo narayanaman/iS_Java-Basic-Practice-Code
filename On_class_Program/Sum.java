509bee22a64818f9e7a7337c77732da262059659
Aman Kushwaha <ashayar630@gmail.com>
2026-07-27 19:35:11 +0530

    Sum Of Iteration

---
+++ On_class_Program/Sum.java
@@ -0,0 +1,9 @@
+public class Sum{
+    public static void main(String []args){
+        int Sum=0;
+        for(int  i=0; i<=10; i++){
+            Sum=Sum+i;
+        }
+        System.out.println("Sum :"+Sum);
+    }
+}
